977ffbd5ee4cf8b71da2207056d52932e5cfd7fd
agui <55447192+BroAgui@users.noreply.github.com>
2022-02-26 17:11:29 +0800

    Add files via upload

---
+++ Garbled/Garbled/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Garbled
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonAll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxIn.Location = new System.Drawing.Point(3, 3);
+            this.textBoxIn.Multiline = true;
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxIn.Size = new System.Drawing.Size(361, 351);
+            this.textBoxIn.TabIndex = 0;
+            this.textBoxIn.TextChanged += new System.EventHandler(this.textBoxIn_TextChanged);
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOut.Location = new System.Drawing.Point(370, 3);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut.Size = new System.Drawing.Size(362, 351);
+            this.textBoxOut.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "解析乱码";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(123, 9);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "生成乱码";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1.编码GBK 解码UTF-8",
+            "2.编码GBK 解码Big5",
+            "3.编码GBK 解码Shift_Jis",
+            "4.编码GBK 解码ISO-8859-1",
+            "5.编码GBK 解码Windows-1252",
+            "6.编码Big5 解码UTF-8",
+            "7.编码Big5 解码GBK",
+            "8.编码Big5 解码Shift_Jis",
+            "9.编码Big5 解码ISO-8859-1",
+            "10.编码Big5 解码Windows-1252",
+            "11.编码UTF-8 解码GBK",
+            "12.编码UTF-8 解码Big5",
+            "13.编码UTF-8 解码Shift_Jis",
+            "14.编码UTF-8 解码ISO-8859-1",
+            "15.编码UTF-8 解码Windows-1252",
+            "16.编码Shift_Jis 解码UTF-8",
+            "17.编码Shift_Jis 解码GBK",
+            "18.编码Shift_Jis 解码Big5",
+            "19.编码Shift_Jis 解码ISO-8859-1",
+            "20.编码Shift_Jis 解码Windows-1252",
+            "21.编码ISO-8859-1 解码UTF-8",
+            "22.编码ISO-8859-1 解码GBK",
+            "23.编码ISO-8859-1 解码Big5",
+            "24.编码ISO-8859-1 解码Shift_Jis",
+            "25.编码ISO-8859-1 解码Windows-1252",
+            "26.编码Windows-1252 解码UTF-8",
+            "27.编码Windows-1252 解码GBK",
+            "28.编码Windows-1252 解码Big5",
+            "29.编码Windows-1252 解码Shift_Jis",
+            "30.编码Windows-1252 解码ISO-8859-1"});
+            this.comboBox1.Location = new System.Drawing.Point(388, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // buttonAll
+            // 
+            this.buttonAll.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonAll.Location = new System.Drawing.Point(633, 5);
+            this.buttonAll.Name = "buttonAll";
+            this.buttonAll.Size = new System.Drawing.Size(83, 23);
+            this.buttonAll.TabIndex = 4;
+            this.buttonAll.Text = "快速预览";
+            this.buttonAll.UseVisualStyleBackColor = true;
+            this.buttonAll.Click += new System.EventHandler(this.buttonAll_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "方式";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(741, 403);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 6;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 230F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel3.Controls.Add(this.radioButton2, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonAll, 5, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label1, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.radioButton1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.comboBox1, 4, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(735, 34);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.textBoxIn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxOut, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 43);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(735, 357);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 403);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(620, 120);
+            this.Name = "Form1";
+            this.Text = "乱码解析器";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ Garbled/Garbled/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Garbled
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void textBoxIn_TextChanged(object sender, EventArgs e)=> textBoxOut.Text = GetText(comboBox1.Text);
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) => textBoxOut.Text = GetText(comboBox1.Text);
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) => textBoxOut.Text = GetText(comboBox1.Text);
+
+        private void buttonAll_Click(object sender, EventArgs e)
+        {
+            textBoxOut.Clear();
+            foreach (string item in comboBox1.Items)
+            {
+                textBoxOut.AppendText(item + "：\r\n");
+                textBoxOut.AppendText( GetText(item) +"\r\n\r\n\r\n");
+            }
+        }
+
+        private string GetText(string mode)
+        {
+            string[] coco = mode.Split(' ');
+            string co1 = coco[0].Split('码')[1];
+            string co2 = coco[1].Substring(2);
+
+            if (radioButton2.Checked)
+            {
+                string tt = co1;
+                co1 = co2;
+                co2 = tt;
+            }
+
+            byte[] ss = Encoding.GetEncoding(co2).GetBytes(textBoxIn.Text);
+            return Encoding.GetEncoding(co1).GetString(ss);
+        }
+    }
+}
